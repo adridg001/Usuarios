268f8b3db798a1dbf039f30d916b957f28b55f3b
Christian Cámara <chcamaraguillen@gmail.com>
2025-01-28 16:54:18 +0100

    cambio para que aparezca el nombre del usuario en el inicio

--- inicio.php
+++ inicio.php
@@ -1,6 +1,18 @@
+<?php
+
+// Verifica si el usuario está logueado
+if (!isset($_SESSION["usuario"])) {
+    header("location:login.php"); 
+    exit();
+}
+
+// Obtén el nombre del usuario desde la sesión
+$nombreUsuario = $_SESSION["usuario"]->nombre;
+?>
+
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Pagina de Inicio</h1>
+    <h1 class="h2">Pagina de Inicio, Bienvenido <?= htmlspecialchars($nombreUsuario) ?></h1>
   </div>
   <div id="contenido">
     <P>PREPARA TUS COMBATES</P>
